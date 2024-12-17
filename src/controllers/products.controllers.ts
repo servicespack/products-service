1f5ccde0fc3b4d7e348ff1520357e2f445118df8
Gabriel Rufino <contato@gabrielrufino.com>
2024-12-17 15:16:01 -0300

    fix: ci

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -6,28 +6,28 @@ export class ProductsControllers {
     private readonly service: ProductsService
   ) {}
 
-  public async create(request: Request, response: Response): Promise<Response> {
+  public async create(request: Request, response: Response): Promise<void> {
     const result = await this.service.create(request.body)
 
-    return result.match(
+    result.match(
       product => response.status(201).json(product),
       err => response.status(400).json({ error: err })
     )
   }
 
-  public async list(request: Request, response: Response): Promise<Response> {
+  public async list(request: Request, response: Response): Promise<void> {
     const result = await this.service.list()
 
-    return result.match(
+    result.match(
       products => response.json({ data: products }),
       err => response.status(400).json({ error: err })
     )
   }
 
-  public async findById(request: Request, response: Response): Promise<Response> {
+  public async findById(request: Request, response: Response): Promise<void> {
     const result = await this.service.findById(request.params.id)
 
-    return result.match(
+    result.match(
       product => response.json(product),
       err => response.status(400).json({ error: err })
     )
